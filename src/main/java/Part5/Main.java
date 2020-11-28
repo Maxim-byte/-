036c8973febdace59da6d63e63796f420dece74d
Maxim-Byte <maxim5gromov@gmail.com>
2020-11-28 15:54:47 +0300

    Resolve annotation policy

--- src/main/java/Part5/Main.java
+++ src/main/java/Part5/Main.java
@@ -6,15 +6,12 @@ import java.lang.reflect.Method;
 public class Main {
 
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        ReflectionClass reflectionClass = new ReflectionClass("Hello");
+        ReflectionClass reflectionClass = new ReflectionClass("Hello _Man _Bro _WHAT");
         Method[] methods = reflectionClass.getClass().getDeclaredMethods();
         for(Method method : methods) {
             method.setAccessible(true);
-            method.invoke(reflectionClass);
             if(method.isAnnotationPresent(Count.class)) {
                 int count = method.getAnnotation(Count.class).count();
-                System.out.println(method.toString());
-                System.out.println(count);
                 for(int i = 0; i < count; ++i) {
                     method.invoke(reflectionClass);
                 }
